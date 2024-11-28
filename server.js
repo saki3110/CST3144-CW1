fefdcfa22f1e48566efa9b45f7396f958d61684f
sakina3110 <129828522+sakina3110@users.noreply.github.com>
2024-11-28 13:07:39 +0400

    Adding a logger

--- server.js
+++ server.js
@@ -1,31 +1,19 @@
 const express = require('express');
-const path = require('path');
 const { connectDB } = require('./mongodb');
-const lessonsRoutes = require('./routes/lessons');
-const ordersRoutes = require('./routes/orders');
-const logger = require('./middleware/logger');
-const staticMiddleware = require('./middleware/static');
 
 const app = express();
 
 // Connect to MongoDB
 connectDB();
 
-// Middleware
-app.use(express.json());
-app.use(logger);
-app.use('/images', staticMiddleware);
-app.use(express.static(path.join(__dirname, '../frontend')));
-
-// API Routes
-app.use('/api/lessons', lessonsRoutes);
-app.use('/api/orders', ordersRoutes);
-
-// Render Frontend
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../frontend/index.html'));
-});
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  if (Object.keys(req.body).length > 0) {
+    console.log("Request Body: ", req.body);
+  }
+  next();
+})
 
 // Start the server
 const PORT = 3000;
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
